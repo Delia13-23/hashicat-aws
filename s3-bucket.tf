c4635b3f91c1460c6d543e7a36ee00445947f17d
Delia De Lira <13delia23@gmail.com>
2023-08-29 06:04:59 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,13 +1,8 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.8.0"
+  # insert the 5 required variables here
   bucket = "my-s3-bucket"
   acl    = "private"
-
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-
-  versioning = {
-    enabled = true
-  }
+  bucket_prefix = ${PREFIX}
 }
